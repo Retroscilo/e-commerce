86031abed8122d0848d9af28578531589fa213cc
Théo BEAURAIN <tho.beaurain@gmail.com>
2022-03-23 11:52:08 +0100

    Add auth fb & github

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <SessionProvider session={pageProps.session} refetchInterval={0}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+export default function MyApp({
+	Component,
+	pageProps: { session, ...pageProps }
+}) {
+	return (
+		<SessionProvider
+			session={pageProps.session}
+			refetchInterval={0}
+		>
+			<Component {...pageProps} />
+		</SessionProvider>
+	);
 }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,39 +1,53 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+import FacebookProvider from "next-auth/providers/facebook";
 import prisma from "../../../lib/prisma.js";
 
 export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-  ],
-  theme: {
-    colorScheme: "light",
-  },
-  callbacks: {
-    async jwt({ token }) {
-      token.userRole = "admin";
-      return token;
-    },
-    async signIn({ account, user }) {
-      if (account.provider === "google") {
-        try {
-          const { email, name } = user;
-          let userInDb = await prisma.user.findUnique({
-            where: {
-              email,
-            },
-          });
-          if (userInDb) return true;
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_ID,
+			clientSecret: process.env.GOOGLE_SECRET
+		}),
+		FacebookProvider({
+			clientId: process.env.FACEBOOK_CLIENT_ID,
+			clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+		}),
+		GitHubProvider({
+			clientId: process.env.GITHUB_CLIENT_ID,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET
+		})
+	],
+	theme: {
+		colorScheme: "light"
+	},
+	callbacks: {
+		async jwt({ token }) {
+			token.userRole = "admin";
+			return token;
+		},
+		async signIn({ account, user }) {
+			if (account.provider === "google"
+				|| account.provider === "github"
+				|| account.provider === "facebook") {
+				try {
+					const { email, name } = user;
+					let userInDb = await prisma.user.findUnique({
+						where: {
+							email
+						}
+					});
 
-          await prisma.user.create({ data: { email, name } });
-          return true;
-        } catch (e) {
-          console.log(e);
-        }
-      }
-    },
-  },
+					if (userInDb) return true;
+
+					await prisma.user.create({ data: { email, name } });
+
+					return true;
+				} catch (e) {
+					console.log(e);
+				}
+			}
+		}
+	}
 });
