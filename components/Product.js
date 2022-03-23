7ff4891c6ba1395bfe79fc2da10603fcc70b8296
Théo BEAURAIN <tho.beaurain@gmail.com>
2022-03-23 09:32:55 +0100

    eslint

--- components/Product.js
+++ components/Product.js
@@ -1,39 +1,39 @@
 import Image from "next/image";
 
 export default function Product({ product, categories }) {
-  const { name, description, price, image } = product;
+	const { name, description, price, image } = product;
 
-  const t = product.categories.map((cat) => cat.categoryId);
-  const category = t.map((id) => categories.filter((c) => c.id === id)).flat();
+	const t = product.categories.map((cat) => cat.categoryId);
+	const category = t.map((id) => categories.filter((c) => c.id === id)).flat();
 
-  return (
-    <div
-      className="max-w-[250px] rounded overflow-hidden shadow-lg"
-      key={product.id}
-    >
-      <Image
-        className="w-full"
-        width={250}
-        height={250}
-        objectFit="cover"
-        src={image}
-        alt={name}
-      />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{name}</div>
-        <p className="text-gray-700 text-base">{description}</p>
-        <p className="text-gray-900 text-xl">${price}</p>
-      </div>
-      <div className="px-6 pt-4 pb-2">
-        {category.map((c, i) => (
-          <span
-            key={i}
-            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-          >
-            {c.name}
-          </span>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className="max-w-[250px] rounded overflow-hidden shadow-lg"
+			key={product.id}
+		>
+			<Image
+				className="w-full"
+				width={250}
+				height={250}
+				objectFit="cover"
+				src={image}
+				alt={name}
+			/>
+			<div className="px-6 py-4">
+				<div className="font-bold text-xl mb-2">{name}</div>
+				<p className="text-gray-700 text-base">{description}</p>
+				<p className="text-gray-900 text-xl">${price}</p>
+			</div>
+			<div className="px-6 pt-4 pb-2">
+				{category.map((c, i) => (
+					<span
+						key={i}
+						className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+					>
+						{c.name}
+					</span>
+				))}
+			</div>
+		</div>
+	);
 }
--- prisma/data.js
+++ prisma/data.js
@@ -1,106 +1,106 @@
 const { Prisma } = require("@prisma/client");
 
 const categories = [
-  {
-    name: "Hats",
-    description: "Things you can wear on your head",
-  },
-  {
-    name: "Socks",
-    description: "Things you can wear on your feet",
-  },
-  {
-    name: "Shirts",
-    description: "Things you wear on the top half of your body",
-  },
+	{
+		name: "Hats",
+		description: "Things you can wear on your head"
+	},
+	{
+		name: "Socks",
+		description: "Things you can wear on your feet"
+	},
+	{
+		name: "Shirts",
+		description: "Things you wear on the top half of your body"
+	}
 ];
 
 const products = [
-  {
-    name: "Cool helmet.",
-    description: "A nice helmet to wear on your head",
-    price: new Prisma.Decimal(19.95),
-    image: "/images/helmet.jpg",
-    quantity: 10,
-    created_at: new Date("2022-01-01"),
-    categories: {
-      create: [
-        {
-          category: {
-            create: {
-              name: "New category",
-              description: "new description category",
-            },
-          },
-        },
-      ],
-    },
-  },
-  {
-    name: "Grey T-Shirt",
-    description: "A nice shirt that you can wear on your body",
-    price: new Prisma.Decimal(22.95),
-    image: "/images/shirt.jpg",
-    quantity: 20,
-    created_at: new Date("2022-01-01"),
-    categories: {
-      create: [
-        {
-          category: {
-            connect: { id: 2 },
-          },
-        },
-        {
-          category: {
-            connect: {
-              id: 3,
-            },
-          },
-        },
-      ],
-    },
-  },
-  {
-    name: "Socks",
-    description: "Cool socks that you can wear on your feet",
-    price: new Prisma.Decimal(12.95),
-    image: "/images/socks.jpg",
-    quantity: 10,
-    created_at: new Date("2022-01-01"),
-    categories: {
-      create: [
-        {
-          category: {
-            connect: {
-              id: 3,
-            },
-          },
-        },
-      ],
-    },
-  },
-  {
-    name: "Sweatshirt",
-    description: "Cool sweatshirt that you can wear on your body",
-    price: new Prisma.Decimal(12.95),
-    image: "/images/sweatshirt.jpg",
-    quantity: 10,
-    created_at: new Date("2022-01-01"),
-    categories: {
-      create: [
-        {
-          category: {
-            connect: {
-              id: 2,
-            },
-          },
-        },
-      ],
-    },
-  },
+	{
+		name: "Cool helmet.",
+		description: "A nice helmet to wear on your head",
+		price: new Prisma.Decimal(19.95),
+		image: "/images/helmet.jpg",
+		quantity: 10,
+		created_at: new Date("2022-01-01"),
+		categories: {
+			create: [
+				{
+					category: {
+						create: {
+							name: "New category",
+							description: "new description category"
+						}
+					}
+				}
+			]
+		}
+	},
+	{
+		name: "Grey T-Shirt",
+		description: "A nice shirt that you can wear on your body",
+		price: new Prisma.Decimal(22.95),
+		image: "/images/shirt.jpg",
+		quantity: 20,
+		created_at: new Date("2022-01-01"),
+		categories: {
+			create: [
+				{
+					category: {
+						connect: { id: 2 }
+					}
+				},
+				{
+					category: {
+						connect: {
+							id: 3
+						}
+					}
+				}
+			]
+		}
+	},
+	{
+		name: "Socks",
+		description: "Cool socks that you can wear on your feet",
+		price: new Prisma.Decimal(12.95),
+		image: "/images/socks.jpg",
+		quantity: 10,
+		created_at: new Date("2022-01-01"),
+		categories: {
+			create: [
+				{
+					category: {
+						connect: {
+							id: 3
+						}
+					}
+				}
+			]
+		}
+	},
+	{
+		name: "Sweatshirt",
+		description: "Cool sweatshirt that you can wear on your body",
+		price: new Prisma.Decimal(12.95),
+		image: "/images/sweatshirt.jpg",
+		quantity: 10,
+		created_at: new Date("2022-01-01"),
+		categories: {
+			create: [
+				{
+					category: {
+						connect: {
+							id: 2
+						}
+					}
+				}
+			]
+		}
+	}
 ];
 
 module.exports = {
-  products,
-  categories,
+	products,
+	categories
 };
